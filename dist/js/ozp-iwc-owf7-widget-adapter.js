2883aaf57aa07963ec9175606cd79e1485129acd
Jason Wagner <jason@nialscorva.net>
2015-04-08 06:35:13 -0400

    fix(shindig): modify the shindig json stringify to use JSON.stringify()

--- dist/js/ozp-iwc-owf7-widget-adapter.js
+++ dist/js/ozp-iwc-owf7-widget-adapter.js
@@ -367,85 +367,10 @@ gadgets.json = function () {
            f(this.getUTCSeconds()), 'Z'].join("");
   };
 
-  // table of character substitutions
-  var m = {
-    '\b': '\\b',
-    '\t': '\\t',
-    '\n': '\\n',
-    '\f': '\\f',
-    '\r': '\\r',
-    '"' : '\\"',
-    '\\': '\\\\'
-  };
-
-  /**
-   * Converts a json object into a string.
-   */
-  function stringify(value) {
-    var a,          // The array holding the partial texts.
-        i,          // The loop counter.
-        k,          // The member key.
-        l,          // Length.
-        r = /["\\\x00-\x1f\x7f-\x9f]/g,
-        v;          // The member value.
-
-    switch (typeof value) {
-    case 'string':
-    // If the string contains no control characters, no quote characters, and no
-    // backslash characters, then we can safely slap some quotes around it.
-    // Otherwise we must also replace the offending characters with safe ones.
-      return r.test(value) ?
-          '"' + value.replace(r, function (a) {
-            var c = m[a];
-            if (c) {
-              return c;
-            }
-            c = a.charCodeAt();
-            return '\\u00' + Math.floor(c / 16).toString(16) +
-                (c % 16).toString(16);
-            }) + '"'
-          : '"' + value + '"';
-    case 'number':
-    // JSON numbers must be finite. Encode non-finite numbers as null.
-      return isFinite(value) ? String(value) : 'null';
-    case 'boolean':
-    case 'null':
-      return String(value);
-    case 'object':
-    // Due to a specification blunder in ECMAScript,
-    // typeof null is 'object', so watch out for that case.
-      if (!value) {
-        return 'null';
-      }
-      // toJSON check removed; re-implement when it doesn't break other libs.
-      a = [];
-      if (typeof value.length === 'number' &&
-          !(value.propertyIsEnumerable('length'))) {
-        // The object is an array. Stringify every element. Use null as a
-        // placeholder for non-JSON values.
-        l = value.length;
-        for (i = 0; i < l; i += 1) {
-          a.push(stringify(value[i]) || 'null');
-        }
-        // Join all of the elements together and wrap them in brackets.
-        return '[' + a.join(',') + ']';
-      }
-      // Otherwise, iterate through all of the keys in the object.
-      for (k in value) if (value.hasOwnProperty(k)) {
-        if (typeof k === 'string') {
-          v = stringify(value[k]);
-          if (v) {
-            a.push(stringify(k) + ':' + v);
-          }
-        }
-      }
-      // Join all of the member texts together and wrap them in braces.
-      return '{' + a.join(',') + '}';
-    }
-  }
-
   return {
-    stringify: stringify,
+    stringify: function(value) {
+        return JSON.stringify(value);
+    },
     parse: function (text) {
 // Parsing happens in three stages. In the first stage, we run the text against
 // regular expressions that look for non-JSON patterns. We are especially
@@ -460,8 +385,7 @@ gadgets.json = function () {
 // open brackets that follow a colon or comma or that begin the text. Finally,
 // we look to see that the remaining characters are only whitespace or ']' or
 // ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
-
-      if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/b-u]/g, '@').
+      if (typeof(text)==="string" && /^[\],:{}\s]*$/.test(text.replace(/\\["\\\/b-u]/g, '@').
           replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
           replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
         return eval('(' + text + ')');
