99c00cfa691fbd70ecc92f81ebba5f2a4be83172
Colin Lowry <lowryc@leidos.com>
2016-10-04 12:56:26 -0700

    Update components.js
    Typo fix.

--- app/js/adapter/owf7Participant/modules/components.js
+++ app/js/adapter/owf7Participant/modules/components.js
@@ -38,7 +38,7 @@ ozpIwc.owf7.participantModules.Components = (function(){
     Components.prototype.onLaunchWidget=function(sender,msg,rpc) {
         msg=JSON.parse(msg);
         // Allow launching by the widget's universal name or by the guid
-        var launchKey = msg.universalName || ms.guid;
+        var launchKey = msg.universalName || msg.guid;
         // ignore title, titleRegex, and launchOnlyIfClosed
         this.systemApi.launch("/application/" + launchKey,{
             contentType: "text/plain",
